6d7e4e054196d11ecebb62f2ae9e4e4bfa3d4e57
Brenton Ashworth <brenton@thinkrelevance.com>
2011-12-04 22:54:22 -0500

    Don't start the fancy logging by default.

--- src/cljs/start/logging.cljs
+++ src/cljs/start/logging.cljs
@@ -1,10 +1,18 @@
 (ns start.logging
-  (:use [library.logging :only (get-logger fancy-output info)])
-  (:require [library.dispatch :as dispatch]))
+  (:require [library.dispatch :as dispatch]
+            [library.logging :as log]))
 
-(def logger (get-logger "events"))
-
-(def fancy (fancy-output "main"))
+(def logger (log/get-logger "events"))
 
 (dispatch/respond-to identity
-                     (fn [t d] (info logger (str (pr-str t) " - " (pr-str d)))))
+                     (fn [t d] (log/info logger (str (pr-str t) " - " (pr-str d)))))
+
+(comment
+
+  ;; log to the console
+  (log/console-output)
+  ;; log to to the "fancy" window
+  (log/fancy-output "main")
+  ;; change the logging level
+  (log/set-level logger :fine)
+  )
