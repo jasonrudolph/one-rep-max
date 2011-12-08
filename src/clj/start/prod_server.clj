147c96713661a1680aadd2f4f08d07aed09b1641
Brenton Ashworth and Craig Andera <pair@thinkrelevance.com>
2011-12-08 10:23:00 -0500

    Add basic authentication to the production server. References #4.

--- src/clj/start/prod_server.clj
+++ src/clj/start/prod_server.clj
@@ -11,13 +11,27 @@
 
 (def root "out/public")
 
+(defn wrap-require-auth
+  [app]
+  (fn [req]
+    (let [denied-response {:headers {"WWW-Authenticate" "Basic realm=\"Restricted\""}
+                           :body "HTTP authentication required."
+                           :status 401}]
+      (if-let [auth ((:headers req) "authorization")]
+        (if (= auth "Basic Y3Nzazppc2F3ZXNvbWU=")
+          ;; username=cssk,password=isawesome
+          (app req)
+          denied-response)
+        denied-response))))
+
 (defroutes app-routes
   remote-routes
-  (ANY "*" request (file-response "404.html" {:root root})))
+  (-> (ANY "*" request (file-response "404.html" {:root root}))
+      (wrap-file root)
+      wrap-file-info
+      wrap-require-auth))
 
 (def app (-> app-routes
-             (wrap-file root)
-             wrap-file-info
              wrap-params))
 
 (defn -main [] (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
