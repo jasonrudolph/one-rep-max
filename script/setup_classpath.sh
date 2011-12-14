4fb6c11e72d6186d027892903496547902105772
Brenton Ashworth and Craig Andera <pair@thinkrelevance.com>
2011-12-14 16:58:38 -0500

    WIP Update scripts to work with new directory structure. Referencs #13.

---
+++ script/setup_classpath.sh
@@ -0,0 +1,10 @@
+if [ "$CLOJURESCRIPT_HOME" = "" ]; then
+  CLOJURESCRIPT_HOME=../clojurescript
+fi
+
+CLJSC_CP='lib/*:lib/dev/*'
+for next in 'lib/*' 'src/clj' 'src/cljs' 'test/cljs'; do
+  CLJSC_CP=${CLJSC_CP}:$CLOJURESCRIPT_HOME'/'$next
+done
+
+CLJSC_CP=$CLJSC_CP':src/app/clj:src/app/cljs:src/app/cljs-macros:src/lib/clj:src/lib/cljs:test:templates'
