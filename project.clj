793569124c24e631226a79d3515c913dc7c2e3e4
fogus <mefogus@gmail.com>
2011-12-16 09:38:04 -0500

    Added Marginialia 0.7.0-SNAP to dev deps

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
                  [ring "1.0.0-RC1"]
                  [compojure "0.6.4"]
                  [enlive "1.0.0"]]
-  :dev-dependencies [[jline "0.9.94"]]
+  :dev-dependencies [[jline "0.9.94"]
+                     [marginalia "0.7.0-SNAPSHOT"]
+                     [lein-marginalia "0.6.1"]]
   :main start.prod-server
   :source-path "src/clj")
