2cfa90880571ce6629c44d823be9903f32aa4f07
fogus <mefogus@gmail.com>
2011-12-19 09:58:07 -0500

    Changed project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurescript-starter-kit "1.0.0-SNAPSHOT"
+(defproject one "1.0.0-SNAPSHOT"
   :description "Getting Started with ClojureScript."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring "1.0.0-RC1"]
