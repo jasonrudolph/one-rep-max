b2518cc9a46ac037b4e8452eb550b5b32c26e459
Brenton Ashworth <brenton@thinkrelevance.com>
2012-01-05 22:21:03 -0500

    Fix logging examples.

--- src/app/cljs/one/sample/logging.cljs
+++ src/app/cljs/one/sample/logging.cljs
@@ -18,9 +18,9 @@ For more information see library.logging."}
 
 (comment
   ;; log to the console
-  (start-display (log/console-output))
+  (log/start-display (log/console-output))
   ;; log to to the "fancy" window
-  (start-display (log/fancy-output "main"))
+  (log/start-display (log/fancy-output "main"))
   ;; change the logging level
   (log/set-level logger :fine)
   )
