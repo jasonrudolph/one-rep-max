e01b9605cf22fd6248381ef994845995d3e4911c
Brenton Ashworth <brenton@thinkrelevance.com>
2012-01-20 14:04:25 -0500

    Remove the need to run ClojureScript's bootstrap script. Refs #34.

--- project.clj
+++ project.clj
@@ -3,10 +3,15 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring "1.0.0-RC1"]
                  [compojure "0.6.4"]
-                 [enlive "1.0.0"]]
+                 [enlive "1.0.0"]
+                 [org.mozilla/rhino "1.7R3"]
+                 [com.google.javascript/closure-compiler "r1592"]
+                 [org.clojure/google-closure-library "0.0-790"]]
   :dev-dependencies [[jline "0.9.94"]
                      [marginalia "0.7.0-SNAPSHOT"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]]
+  :repositories {"oss-sonatype-staging"
+                 "https://oss.sonatype.org/content/groups/staging/"}
   :main ^:skip-aot one.sample.launchpad
   :source-path "src/app/clj"
   :extra-classpath-dirs ["lib/clojurescript/src/clj"
