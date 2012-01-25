b5d7f0f25cb5e2ea62a9a7c274680c39c451deb2
Murphy McMahon <pandeiro@gmail.com>
2012-01-25 18:16:23 -0200

    add construct-html to snippets' call chain so _include tags are processed

--- src/app/cljs-macros/one/sample/snippets.clj
+++ src/app/cljs-macros/one/sample/snippets.clj
@@ -1,11 +1,11 @@
 (ns one.sample.snippets
   "Macros for including HTML snippets in the ClojureScript application
   at compile time."
-  (:use [one.templates :only (render)])
+  (:use [one.templates :only (render construct-html)])
   (:require [net.cgrand.enlive-html :as html]))
 
 (defn- snippet [file id]
-  (render (html/select (html/html-resource file) id)))
+  (render (html/select (construct-html (html/html-resource file)) id)))
 
 (defmacro snippets
   "Expands to a map of HTML snippets which are extracted from the
