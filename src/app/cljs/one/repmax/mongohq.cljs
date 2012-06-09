d27000a139f470cf321a7e92a4b29301ccdcf02a
Jason Rudolph <github@jasonrudolph.com>
2012-06-09 10:51:31 -0400

    Enhance #find-documents to support a 'limit' arg
    So far, because the list of exercise documents is fairly small (~150),
    and because we want to show all exercises in the exercise list, we have
    always fetched _all_ documents whenever querying a MongoHQ collection.
    
    Soon, we want to start fetching the list of _recent_ exercise sets.
    Exercises will have a _lot_ of sets, and we don't want the overhead of
    fetching _all_ sets when we only want the recent ones. Now that we can
    limit the number of documents returned by #find-documents, we are part
    of the way toward our goal of fetching a limited number of recent
    exercise sets. Next, we'll need to enhance #find-documents to support a
    'sort' argument. We'll combine that argument with the 'limit' argument
    to allow us to fetch a limited number of recent exercise sets.

--- src/app/cljs/one/repmax/mongohq.cljs
+++ src/app/cljs/one/repmax/mongohq.cljs
@@ -137,77 +137,102 @@
   "Finds all documents in the given collection. Invokes the given callback,
    passing in a sequence of documents (as Clojure maps).
 
+   Optional keyword argument:
+   * :limit The total number of documents to fetch. Defaults to infinity (i.e.,
+            fetches all documents in this collection).
+
    MongoHQ returns a maximum of 100 documents per request. If the given collection
    has more than 100 documents, this function makes multiple requests to MongoHQ.
    It will make as many requests as needed to fetch all the documents in the
    collection.
   "
-  [api-key collection-name on-success]
+  [api-key collection-name on-success & {:keys [limit]
+                                         :or   {limit js/Infinity}}]
   (let [url (root-documents-url collection-name)]
-    (find-documents-request api-key url :on-success on-success)))
+    (find-documents-request api-key
+                            url
+                            :limit-overall limit
+                            :on-success on-success)))
+
+(def ^{:doc "The maximim number of documents that MongoHQ will return in a given request.
+             When querying for documents, MongoHQ uses this value as the default value
+             (and the maximum allowed value) for the 'limit' parameter."}
+  max-docs-per-request 100)
 
 ; TODO Add support for on-error callback
 (defn- find-documents-request
   "Issues a request to the given URL to find documents.
 
    Optional keyword argments:
-   * :skip          Used for pagination in MongoHQ. Indicates the number of documents to
-                    'skip' over when determining which documents to return from a query.
-                    Defaults to 0.
-   * :limit         The maximum number of documents to return in this request.
-                    Defaults to 100 (i.e., the maximum limit supported by MongoHQ).
-   * :previous-docs A sequence of documents produced by previous calls to this same
-                    function. Since we may need to call MongoHQ multiple times to 'paginate'
-                    through the list of documents, we can pass the results from previous calls
-                    to subsequent calls. After the final request to MongoHQ, all results are
-                    combined and passed to the on-success function.
-                    Defaults to an empty Vector.
-   * :on-success    The function that will be invoked upon successful completion of the
-                    request. The function must accept a single argument: a sequence of
-                    documents.
+   * :skip              Used for pagination in MongoHQ. Indicates the number of documents to
+                        'skip' over when determining which documents to return from a query.
+                        Defaults to 0.
+   * :limit-per-request The maximum number of documents to return in this request. Defaults
+                        to max-docs-per-request (i.e., the maximum limit supported by MongoHQ).
+   * :limit-overall     The total number of documents being requested.
+   * :previous-docs     A sequence of documents produced by previous calls to this same
+                        function. Since we may need to call MongoHQ multiple times to 'paginate'
+                        through the list of documents, we can pass the results from previous calls
+                        to subsequent calls. After the final request to MongoHQ, all results are
+                        combined and passed to the on-success function.
+                        Defaults to an empty Vector.
+   * :on-success        The function that will be invoked upon successful completion of the
+                        request. The function must accept a single argument: a sequence of
+                        documents.
   "
-  [api-key url & {:keys [skip limit previous-docs on-success]
+  [api-key url & {:keys [skip limit-per-request limit-overall previous-docs on-success]
                   :or   {skip 0
-                         limit 100
+                         limit-per-request max-docs-per-request
+                         limit-overall js/Infinity
                          previous-docs []
                          on-success (fn [data])}}]
-  (request (with-params url {:_apikey api-key, :skip skip, :limit limit})
+  (request (with-params url {:_apikey api-key, :skip skip, :limit limit-per-request})
            :on-success (find-documents-callback api-key
                                                 url
                                                 skip
-                                                limit
+                                                limit-per-request
+                                                limit-overall
                                                 previous-docs
                                                 on-success)))
 
 (defn- find-documents-callback
-  "Returns a callback function to be invoked after a successful request to MongoHQ to find
-   documents for a collection.
+  "Returns a callback function to be invoked after a successful request to
+   MongoHQ to find documents for a collection.
 
-   Ensures that we get *all* documents in the collection before we invoke the original
-   callback provided by the caller. To do so, it determines whether the another request is
-   needed in order to fetch more documents.
+   Ensures that we get the requested number of documents (as specified by the
+   limit-overall argument) *or* all documents in the collection (whichever is
+   smaller) before we invoke the original callback provided by the caller. To do
+   so, it determines whether the another request is needed in order to fetch
+   more documents.
 
-   If this request returned fewer documents than allowed by the given limit, then we know
-   that we have reached the end of the 'resultset' for this query. In this case, we combine
-   the previous results with the results from this request, and we pass the combined results
-   to the original callback provided by the caller.
+   If we have received the overall requested number or documents
+   (limit-overall), *or* if this specific request returned fewer documents than
+   requested (limit-per-request), then we know that we have reached the end of
+   the 'resultset' for this query. In this case, we combine the previous results
+   with the results from this request, and we pass the combined results to the
+   original callback provided by the caller.
 
-   On the other hand, if this request returned the number of documents specified in the
-   limit, then there may be other documents remaining in the 'resultset' for this query.
-   In this case, we issue a subsequent request to MongoHQ for the next 'page' of results
-   in this 'resultset'. We continue this pattern until we have fetched all documents in
-   the collection.
+   On the other hand, if this request returned the number of documents
+   specified in limit-per-request, then there may be other documents remaining
+   in the 'resultset' for this query.  In this case, we issue a subsequent
+   request to MongoHQ for the next 'page' of results in this 'resultset'. We
+   continue this pattern until we have fetched the requested number of documents
+   (limit-overall) or all documents in the collection (whichever is smaller).
   "
-  [api-key url skip limit previous-docs on-success]
+  [api-key url skip limit-per-request limit-overall previous-docs on-success]
   (fn [new-docs]
-    (let [all-docs (apply conj previous-docs new-docs)]
-      (if (< (count new-docs) limit)
-        (on-success (documents->maps all-docs))
+    (let [docs (apply conj previous-docs new-docs)
+          fetched-all-docs? (or
+                              (>= (count docs) limit-overall)
+                              (< (count new-docs) limit-per-request))]
+      (if fetched-all-docs?
+        (on-success (take limit-overall (documents->maps docs)))
         (find-documents-request api-key
                                 url
-                                :skip (+ skip limit)
-                                :limit limit
-                                :previous-docs all-docs
+                                :skip (+ skip limit-per-request)
+                                :limit-per-request limit-per-request
+                                :limit-overall limit-overall
+                                :previous-docs docs
                                 :on-success on-success)))))
 
 (defn create-document [api-key collection-name document on-success on-error]
